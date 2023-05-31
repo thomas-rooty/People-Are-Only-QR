97b883c796f036f3e54d5367c8859afc3b0ce42e
tcaron <tc.caron91@gmail.com>
2023-05-31 16:40:58 +0200

    Styled detail OnePeople

--- screens/onepeople/OnePeople.js
+++ screens/onepeople/OnePeople.js
@@ -1,85 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import { Text, View, StyleSheet, Button, Image } from 'react-native'
+import React from 'react'
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 const OnePeople = ({ route }) => {
   const { personData } = route.params
-  
+
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{
-          uri: personData.picture?.large,
-        }}
-      />
-        <Text>
+      <View style={styles.onePersonCard}>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={{
+              uri: personData.picture?.large,
+            }}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.name}>
             {personData.name?.first} {personData.name?.last}
-        </Text>
-        <Text style={styles.email}>
-            {personData.email}
-        </Text>
-        <Text>
-            {personData.location?.city}
-        </Text>
-        <Text>
-            {personData.location?.country}
-        </Text>
-        <Text>
-            {personData.location?.postcode}
-        </Text>
-        <Text>
-            {personData.location?.state}
-        </Text>
-        <Text>
-            {personData.location?.street?.name}
-        </Text>
-        <Text>
-            {personData.location?.street?.number}
-        </Text>
-        <Text>
-            {personData.phone}
-        </Text> 
-        <Text>
-            {personData.cell}
-        </Text>
-        <Text>
-            {personData.dob?.age} ans
-        </Text>
-        <Text>
-            {personData.dob?.date}
-        </Text>
+          </Text>
+          <Text style={styles.email}>{personData.email}</Text>
+          <Text>{personData.dob?.age} years old</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#e3e3e3',
-      },
-    imageContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    image: {
-      width: 50,
-      height: 50,
-      borderRadius: 50,
-    },
-    textContainer: {
-      flex: 3,
-      justifyContent: 'center',
-      alignItems: 'flex-start',
-    },
-    name: {
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    email: {
-      fontSize: 14,
-      color: '#888',
-    },
-  })
+  container: {
+    flex: 1,
+    backgroundColor: '#ffcd00',
+    padding: 20,
+    paddingTop: 50,
+  },
+  onePersonCard: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: '#ffba00',
+    shadowOffset: { width: 10, height: 10 },
+    shadowOpacity: 1,
+    shadowRadius: 0,
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+  textContainer: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  email: {
+    fontSize: 18,
+    color: '#888',
+    marginBottom: 10,
+  },
+})
 
 export default OnePeople
